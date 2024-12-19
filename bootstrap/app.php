7e79b01264eeef7dda30af80fa8fdd1a176aab98
Jkanpariy2004 <jainishkanpariya@gmail.com>
2024-12-19 20:25:46 +0530

    first commit

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Middleware\VerifyJWTToken;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -13,9 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->alias([
-            'auth.check'=>VerifyJWTToken::class,
-        ]);
+        //
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
